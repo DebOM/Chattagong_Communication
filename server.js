c76be914e39af82c32a396bf01ce2a14d1f29239
Debasish Mozumder <debasishbd@hotmail.com>
2017-07-06 17:49:50 -0400

    chat interaction is woking using htm & jquery on FE

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var users  = [];
-var connection = [];
+var connections = [];
 
 server.listen(process.env.PORT || 3000);
-console.log('server running....')
+console.log('server running....');
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -16,6 +17,28 @@ io.sockets.on('connection', function(socket){
   console.log('Connected: %s sockets connected', connections.length);
 
   //Disconnect
-  connnections.splice(connections.indexOf(socket), 1)
-  console.log('Disconnected: %s socket connected', connections.length);
+  socket.on('disconnect', function(data){
+    if(!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updatausers();
+
+    connections.splice(connections.indexOf(socket), 1)
+    console.log('Disconnected: %s socket connected', connections.length);
+  });
+  //send Message
+  socket.on('send message', function(data){
+    io.sockets.emit('new message', {msg: data, user: socket.username});
+  });
+
+  //new users\
+  socket.on('new user', function(data, callback){
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updatausers();
+  });
+
+  function updatausers(){
+    io.sockets.emit('get users', users);
+  }
 });
