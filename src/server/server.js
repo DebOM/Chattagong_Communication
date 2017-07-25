716c6bc3faa61c4b067f1e9a2a26fe8dbfedd6ba
Debasish Mozumder <debasishbd@hotmail.com>
2017-07-24 20:11:30 -0400

    reprogrammed socket functions client supportDesk in the client working

--- src/server/server.js
+++ src/server/server.js
@@ -1,45 +1,60 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io').listen(server);
+const express = require('express');
+const path = require('path');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
 
 server.listen(process.env.PORT || 8080);
 console.log('server running....ON PORT 8080');
 
 app.use(express.static(path.join(__dirname, '../client')));
 
-// var ns1 = io.of('/ns1'); // this is a custom defined namespace;
+// let ns1 = io.of('/ns1'); // this is a custom defined namespace;
 
-//connection establised on ns1 namespace;
+let usernames = {};
+let clientsRooms = [];
+let helpDeskRooms = [];
 
-var usernames = {};
-var rooms = [];
+io.on('connection', socket => { //CONNECTION ESTABLISHED ON DEFAULT NAMESPACE;
 
-io.on('connection', socket => {
-  rooms.push(socket);
-  socket.on('message', message => {
+  let clientRoomsData = clientsRooms.map(room => ({id: room.id})); //THIS IS FOR SupportDesk TO USE, ADD PROPERTY TO THIS OBJECT AS NEEDED IN THE CLIENT SIDE
+
+  socket.on('add client to rooms', (client, callback) => { //THIS EVENT ADD THE SOCKET TO CLIENTROOMS IF NAME IS VALID
+    if(client.length > 0){
+      let thisSocket = socket;
+      thisSocket.username = client;
+      callback(true);
+      clientsRooms.push(thisSocket);
+      // io.emit('clients', { activeClients: clientRoomsData })
+      console.log("just added client to room , now " + clientsRooms.length)
+    }else{
+      callback(false);
+    }
+  });
+
+  socket.on('message', message => { //THIS EVENT HANDLE MESSAGE COMING IN AND OUT
     io.emit('message', {
       body: message.body,
       from: message.from,
       time: message.time,
       img: null,
-      // from: socket.id.slice(8),
-      // user: socket.username
-    });
-    console.log("server side ," + rooms.length)
-  });
-
-  socket.on('get clients', data => {
-    var roomInfo = rooms.map(room => {
-      return {id: socket.id, userName: socket.username};
-    })
-    socket.emit('get clients',{
-      activeClients: "roomInfo",
     })
   });
 
-  socket.on('disconnect', () => {
-    console.log(socket.id + ", this socket is disconnected")
+  socket.on('get clients', data => { //THIS EVENT MADE SPECIFICALY FOR SUPPORTDESK TO DISPLAY ALL ACTIVE CLIENT ROOMS
+    console.log('inside get clients!!!!')
+    console.log('inside get clients! total clients ,' + clientsRooms.length)
+    io.emit('clients', {
+      activeClients: clientRoomsData,
+    })
   });
+
+  socket.on('disconnect', () => { //THIS EVENT REMOVES THE SOCKET FROM CLIENTS ROOM AS THEY LEAVE OR CLOSE CHAT WINDOW
+      console.log("total room before filter , " + clientsRooms.length)
+      console.log(socket.id + ", this socket is disconnected")
+      clientsRooms = clientsRooms.filter(room => {
+        return room.id !== socket.id;
+      })
+      console.log("total room after filter , " + clientsRooms.length)
+  })
 });
