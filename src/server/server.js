1e052324d28f92c3c80363f7fb7a5c06a6831223
Debasish Mozumder <debasishbd@hotmail.com>
2017-08-02 14:01:39 -0400

    code cleanup on server.js

--- src/server/server.js
+++ src/server/server.js
@@ -23,20 +23,13 @@ io.on('connect', socket => {
   socket.on('add client to rooms', (client, callback) => {
     console.log('step one', client)
     if (client.length > 0) {
-      // let thisSocket = socket;
       let newRoom = client+'-'+socket.id.slice(-6);
       socket.room = newRoom;
-      // thisSocket.username = client;
       socket.clientName = client;      
-      ///JUST ADDED THIS LINE, JUST INCASE CANT JOIN ROOM THROUGH ID;
-      // thisSocket.room = client+socket.id.slice(-4);
-      // clientsRooms.push(thisSocket);
       let newClient= {clientName: client, roomId: newRoom, socketId: socket.id };
       console.log("step two new client obj", newClient)
-      // clientsInQueue[client] = newClient;
       clientsInQueue.push(newClient);
       socket.join(newRoom);
-      // clientRoomsData = clientsRooms.map(room => ({id: room.id, clientName: room.username, room: null}));
       console.log('total number clientsInQueue ,', clientsInQueue, clientsInQueue.length);
       callback(true);
       io.emit('clientsInQueue', { clients: clientsInQueue });
@@ -47,10 +40,8 @@ io.on('connect', socket => {
 
   //THIS EVENT MADE SPECIFICALY FOR HELPDESK TO DISPLAY ALL CLIENT ROOMS IN QUEUE AND IN CONVERSATION
   socket.on('get clients', data => {
-    // clientRoomsData = clientsInQueue.map(room => ({id: room.id, clientName: room.username}));
-      console.log('total number clientsInQueue ,', clientsInQueue, clientsInQueue.length);    
-    //clientRoomsData IS FOR helpDesk TO USE, ADD PROPERTY TO THIS OBJECT AS NEEDED FOR THE CLIENT SIDE
-      io.emit('clientsInQueue', { clients: clientsInQueue });    //????clientsRooms[clientsRooms.length-1]
+    //NEXT TWO LINE IS FOR helpDesk TO USE, ADD PROPERTY TO THIS OBJECT AS NEEDED FOR THE CLIENT SIDE
+      io.emit('clientsInQueue', { clients: clientsInQueue });
       io.emit('clientsInCoversation', { clients: clientsInCoversation });
   });
 
@@ -62,9 +53,8 @@ io.on('connect', socket => {
     console.log('total number helpdesk rooms ,' + allActiveHelpDesk.length);
   });
 
+  //THIS EVENT JOINS A HELPDESK TO A CLIENT ROOM, ALSO IT UPDATES THE STATE OF CLIENTS ON THE CLIENT SIDE
   socket.on('join helpDesk to room', (data, callback) => {  
-    // this.socket.emit('update queue State', client);
-    // this.socket.emit('update inConvers State', client);
     let roomToJoin = data.roomId;
     socket.room = roomToJoin;
     socket.join(roomToJoin);
@@ -74,7 +64,7 @@ io.on('connect', socket => {
       time: null,
       img: null,
     }
-    
+    //NEXT TWO LINES CHANGES THE CURRENT STATE OF CLIENT, REMOVES FROM INQUEUE ARRAY TO IN CONVO ARRAY
     clientsInQueue = clientsInQueue.filter( inQueue => inQueue.socketId !== data.socketId);
     clientsInCoversation.push(data)
     io.emit('clientsInQueue', { clients: clientsInQueue });    
@@ -103,7 +93,7 @@ io.on('connect', socket => {
     })
   });
 
-//THIS EVENT REMOVES THE SOCKET FROM CLIENTS ROOM AS THEY LEAVE OR CLOSES CHAT WINDOW
+//THIS EVENT REMOVES THE SOCKET FROM CLIENTS ROOMs AS THEY LEAVE OR CLOSES CHAT WINDOW
   socket.on('disconnect', () => {
     if (socket.clientName) {
       console.log(socket.id + ", this socket is disconnected from clientsInQueue")
